5b13cfab2e44a50b1b2f69d81a677de191be59ea
spencer-tan <spencerltan@gmail.com>
2018-10-29 12:15:42 -0500

    testing commits and pushes

--- assignment1/src/cs335/Main.java
+++ assignment1/src/cs335/Main.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         File file = new File("./src/CS335/test.txt"); //create file
-        int verticies = 0; //number of verticiees
+        int verticies = 0; //number of verticies
 
         Graph graph = new Graph(); //create a graph
         try {
